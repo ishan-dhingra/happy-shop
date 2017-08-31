4630f52d7d36b203b56cdcf0eae86cf0e9eb0241
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-08-31 20:00:28 +0530

    CategoryListViewModel loadCategories Green

--- app/src/main/java/com/anythingintellect/happyshop/viewmodel/CategoryListViewModel.java
+++ app/src/main/java/com/anythingintellect/happyshop/viewmodel/CategoryListViewModel.java
@@ -1,6 +1,10 @@
 package com.anythingintellect.happyshop.viewmodel;
 
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableList;
+
 import com.anythingintellect.happyshop.db.LocalDataStore;
+import com.anythingintellect.happyshop.model.Category;
 import com.anythingintellect.happyshop.repo.CatalogRepository;
 
 /**
@@ -9,12 +13,19 @@ import com.anythingintellect.happyshop.repo.CatalogRepository;
 
 public class CategoryListViewModel {
 
+    private final CatalogRepository repository;
+    private final ObservableList<Category> categories;
 
-    public CategoryListViewModel(CatalogRepository localStore) {
-
+    public CategoryListViewModel(CatalogRepository catalogRepository) {
+        this.repository = catalogRepository;
+        this.categories = new ObservableArrayList<>();
     }
 
     public void loadCategories() {
+        categories.addAll(repository.getCategories());
+    }
 
+    public ObservableList<Category> getCategories() {
+        return categories;
     }
 }
