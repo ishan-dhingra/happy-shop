084e745a93fba48d12fc514716ef4097ff46ba1c
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-08-31 16:53:35 +0530

    CartRepository addToCart Red

---
+++ app/src/main/java/com/anythingintellect/happyshop/repo/CartRepository.java
@@ -0,0 +1,20 @@
+package com.anythingintellect.happyshop.repo;
+
+import com.anythingintellect.happyshop.db.LocalDataStore;
+
+/**
+ * Created by ishan.dhingra on 31/08/17.
+ */
+
+public class CartRepository {
+
+    private final LocalDataStore localStore;
+
+    public CartRepository(LocalDataStore localStore) {
+        this.localStore = localStore;
+    }
+
+    public void addToCart(long productId) {
+
+    }
+}
---
+++ app/src/test/java/com/anythingintellect/happyshop/repo/CartRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.anythingintellect.happyshop.repo;
+
+import com.anythingintellect.happyshop.BaseTest;
+import com.anythingintellect.happyshop.db.LocalDataStore;
+import com.anythingintellect.happyshop.model.CartEntry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by ishan.dhingra on 31/08/17.
+ */
+
+public class CartRepositoryTest extends BaseTest {
+
+    @Mock
+    private LocalDataStore localStore;
+    private CartRepository cartRepository;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        cartRepository = new CartRepository(localStore);
+    }
+
+
+    // addToCart
+    // Should add to local store cart
+    @Test
+    public void testAddToCart_ShouldCallLocalStoreToSave() {
+       long productId = 1000L;
+        cartRepository.addToCart(productId);
+        verify(localStore).addToCart(productId);
+    }
+
+
+
+    // getFromCart
+    // Should get from local store cart
+
+
+
+}
